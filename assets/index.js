bf632094c8e01314895f2b9e55cf5dc2063b13ef
Kazungu1 <kazunguderriq@gmail.com>
2022-06-23 11:52:26 +0300

    add and style the copy button

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,20 @@
-console.log("welcome to moringa school")
+const text=document.querySelector(".quote");
+const author=document.querySelector(".author-name");
+const nextBtn =document.querySelector(".next-quote");
+const tweetButton = document.querySelector(".twitter-share-button")
+const getQuote = async () => {
+  const res = await fetch("https://type.fit/api/quotes");
+  const quotes = await res.json();
+  const num = Math.floor(Math.random() * quotes.length);
+  const item = quotes[num];
+  const quote = item.text;
+  const authorName =item.author;
+  text.innerText = quote;
+  author.innerText = authorName;
+  tweetButton.href=`https://twitter.com/intent/tweet?text=${quote}-${authorName}`
+
+
+//   console.log(item);
+};
+nextBtn.addEventListener('click',getQuote)
+getQuote();
